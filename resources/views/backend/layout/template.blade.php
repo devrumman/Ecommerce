290c26571bc63b3c2613bbee4e33592e49940ab7
Mohmudul Hassan Rumman <rummanahmed1991@gmail.com>
2022-01-04 04:16:58 +0600

    Signed-off-by: Mohmudul Hassan Rumman <rummanahmed1991@gmail.com>

---
+++ resources/views/backend/layout/template.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  @include ('backend.includes.header')
+  @include ('backend.includes.css')
+
+  
+</head>
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+
+<div class="wrapper">
+
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__wobble" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+  </div>
+
+  @include ('backend.includes.topbar')
+
+  @include ('backend.includes.nav')
+
+
+  <!--- Body Content Start --->
+  @yield ('adminBodyContant')
+  <!--- Body Content End   --->
+
+
+  @include ('backend.includes.footer')
+
+
+</div>
+<!-- ./wrapper -->
+
+  @include ('backend.includes.script')
+  
+</body>
+</html>
---
+++ resources/views/backend/pages/brands/index.blade.php
@@ -0,0 +1,35 @@
+@extends ('backend.layout.template')
+
+@section ('adminBodyContant')
+
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Brand Page</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Brand Page</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+
+
+	<section class="content">
+	    <div class="container-fluid">
+	      <!-- Info boxes -->
+	      <div class="row">
+	        <div class="col-12 col-sm-6 col-md-3">
+	        	This Is The Brand Page
+	        </div>
+	      </div>
+	    </div>
+	</section>    
+</div>        
+@endsection
